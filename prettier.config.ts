7130f35263ea0e98d4240c18072140f9dde6a9da
jawade.arpeet <jawade.arpeet@gmail.com>
2025-06-28 22:55:40 +0530

    init

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+const config = {
+  singleQuote: false,
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  arrowParens: "always",
+  bracketSpacing: true,
+  useTabs: false,
+  singleAttributePerLine: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
